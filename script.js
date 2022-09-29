e6ddd09d0a9aa38bf413ece33ca7e078df0275ff
razen7 <hopeful.raz@gmail.com>
2022-09-29 17:50:05 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const containerEl = document.querySelector(".container");
+
+function showNav() {
+    containerEl.classList.add('show-nav');
+}
+function hideNav() {
+    containerEl.classList.remove('show-nav');
+}
